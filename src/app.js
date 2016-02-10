d10d5dcb13c2573eb3bb730fb3b97f5e7e8f5395
Tim van Lent <lentvantim@hotmail.com>
2016-02-10 14:50:48 +0100

    bulletin board

---
+++ src/app.js
@@ -0,0 +1,86 @@
+var express = require('express');
+var fs = require('fs');
+var app = express();
+var bodyParser = require('body-parser');
+var http = require('http');
+var pg = require('pg');
+
+app.set('views', './src/views');
+app.set('view engine', 'jade');
+
+app.get('/', function(req, res) {
+	res.render('index')
+});
+
+console.log("hello")
+app.post('/', bodyParser.urlencoded({
+			extended: true
+		}),
+		function(req, res) {
+
+			
+			var connectionString = "postgres://timvanlent@localhost/classexample";
+			pg.connect(connectionString, function(err, client, done) {
+				if (err) {
+					if (client) {
+						done(client);
+					}
+					return;
+				}
+			
+
+					client.query(
+					'INSERT into messages(title, body) VALUES($1, $2) RETURNING id', [req.body.titel, req.body.bodie],
+					function(err, result) {
+						if (err) {
+							done(client);
+							return;
+							console.log(err)
+						} else {
+							done();
+							console.log('didit')
+						}
+						console.log('hellaef');
+						console.log(result.rows)
+						res.redirect('messages')
+					});
+				});
+
+			});
+			
+		
+			
+
+			app.get('/messages', function (req,res){
+			var connectionString = "postgres://timvanlent@localhost/classexample";
+			pg.connect(connectionString, function(err, client, done) {
+				if (err) {
+					if (client) {
+						done(client);
+					}
+					return;
+				}
+					client.query('select * from messages', function(err, result) {
+					if (err) {
+						done(client);
+						return;
+					} else {
+						done();
+					}
+					console.log('gotta gitget');
+					console.log(result.rows);
+					
+					
+					res.render('messages',{
+					messages: result.rows 
+					});
+
+				});
+
+				
+			});
+	});
+			var server = app.listen(3000);
+
+
+
